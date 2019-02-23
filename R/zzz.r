9a932ca477de803c58e01baccf93decd8d2d1155
Gibran Hemani <explodecomputer@gmail.com>
2019-02-23 19:14:41 +0000

    upd

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
 
-	options(eveneo4jurl="http://shark.epi.bris.ac.uk:8474/db/data")
+	options(eveneo4jurl="http://eve-neo4j.mrbase.org/db/data")
 	options(eveneo4juser="mrever")
 	options(eveneo4jpw="mrever")
 	message("Run connect() to get started")
